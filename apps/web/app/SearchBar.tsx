9baeb37bc6262de1b4174368af5ce7f5794bbaf1
danielpadkins <danielpadk@gmail.com>
2024-01-20 17:33:59 -0500

    dropdown style

--- apps/web/app/SearchBar.tsx
+++ apps/web/app/SearchBar.tsx
@@ -22,17 +22,17 @@ export const SearchBar = ({ className, ...props }: any) => {
   const handleChange = (event: any) => setInputValue(event.target.value);
 
   return (
-    <div>
+    <div className="w-[500px]">
       <Input
         type="text"
         placeholder="Enter a search"
-        className="w-[500px]"
+        className="w-full"
         onFocus={handleFocus}
         onBlur={handleBlur}
         onChange={handleChange}
       />
       {isFocused && !inputValue && (
-        <div className="w-[500px] absolute mt-1 bg-beige-50 text-sm flex flex-col gap-2 z-50 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
+        <div className="w-[500px] absolute mt-1 bg-beige-50 text-sm flex flex-col z-50 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
           {SUGGESTED_SEARCHES.map((search) => (
             <span className="hover:bg-neutral-100 p-2 rounded-md cursor-pointer">
               {search}
