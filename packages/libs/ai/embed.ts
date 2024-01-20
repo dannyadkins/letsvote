6df0d435954a56844c9a41d5bd5b3825e7114052
danielpadkins <danielpadk@gmail.com>
2024-01-20 17:45:29 -0500

    init libs

---
+++ packages/libs/ai/embed.ts
@@ -0,0 +1,26 @@
+export async function getEmbedding(
+  inputText: string,
+  model = "text-embedding-ada-002"
+) {
+  const url = "https://api.openai.com/v1/embeddings";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      input: inputText,
+      model: model,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data?.data?.[0]?.embedding;
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
