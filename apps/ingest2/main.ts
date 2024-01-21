cebc3598f2bcb7d547f99e872c728839a155da04
danielpadkins <danielpadk@gmail.com>
2024-01-21 17:03:52 -0500

    migrate to bun

--- apps/ingest2/main.ts
+++ apps/ingest2/main.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+require("dotenv").config();
+
+import prisma from "db";
+
 import { TwitterScraperClient } from "./clients/twitter/client";
 
 // main func
 async function main() {
   console.log("main");
   const twitterClient = new TwitterScraperClient("Nikki Haley");
-  const dbClient = new PrismaClient();
+  const dbClient = prisma;
   await twitterClient.run(dbClient);
 }
 
