65e8dc9634c7967805dbe97212624e31133c50c2
danielpadkins <danielpadk@gmail.com>
2024-02-04 16:27:25 -0500

    add llm

--- apps/py-ingest/llm.py
+++ apps/py-ingest/llm.py
@@ -3,6 +3,12 @@
 from abc import ABC, abstractmethod
 import os
 import openai
+import instructor
+from openai import OpenAI
+from pydantic import BaseModel
+
+# Enables `response_model`
+
 
 class AbstractLLM(ABC):
     @abstractmethod
@@ -12,10 +18,13 @@ class AbstractLLM(ABC):
 
 class GPT(AbstractLLM):
     def __init__(self, version: str, system_prompt: str = None):
-        self.client = openai.OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        openai = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        client = instructor.patch(OpenAI())
+        self.client = client
+        
         self.model_version = "gpt-3.5-turbo" if self.version == "3.5" else "gpt-4-turbo"
 
-    def generate(self, prompt: str):
+    def generate(self, prompt: str, response_model: BaseModel = None, functions: list = []):
         messages = [{"role": "user", "content": prompt}]
         if self.system_prompt:
             messages.insert(0, {"role": "system", "content": self.system_prompt})
@@ -23,6 +32,8 @@ class GPT(AbstractLLM):
         response = self.client.chat.completions.create(
             model=self.model_version,
             messages=messages,
+            functions=functions,
+            response_model=response_model,
             temperature=0.8,
             max_tokens=64,
             top_p=1
